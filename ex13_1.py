3a0afafb0957d9cc1d784eaf3bbeb0077e30855e
kocsis931122 <achilleus88888@gmail.com>
2015-11-15 17:44:04 +0100

    40 exercises

---
+++ ex13_1.py
@@ -0,0 +1,10 @@
+from sys import argv
+
+script, two, three, four, five = argv
+
+script = input("The script is called: ")
+two = input("Your first variable is: ")
+three = input("Your second variable is: ")
+four = input("Your third variable is: ")
+five = input("Your fourth variable is: ")
+
---
+++ ex13_2.py
@@ -0,0 +1,7 @@
+from sys import argv
+
+script, first, second, third = argv
+fourth = input("What is your fourth variable? ")
+
+print("All together, your script is called %r, your first variable is %r, your second is %r, your third is %r, and your fourth is %r" % (script, first, second, third, fourth))
+
